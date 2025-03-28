7c3d0c9f41e01ac360aad68f14457782bb1c1681
Samuel Rozini Hirt <Samuel_rozini@hotmail.com>
2025-03-28 20:30:12 -0300

    Add files via upload

---
+++ Uniavan/Semestre 2/Algoritmos 2/Projetos/PonteiroSimples/PonteiroSimples.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int a, b;
+    a = 5;
+
+    b = a;
+    a = 8;
+    printf("\n a = %d, b = %d", a, b);
+
+    a = b = 5;
+    a = 8;
+    printf("\n a = %d, b = %d", a, b);
+    printf("\n a = %d (%d), b = %d (%x)", a, &a, b, &b);
+
+    a = 5;
+
+    int *pti = NULL;
+    pti = &a; //recebe o endereço da variavel chamada 'a'
+    a = 8;
+    printf("\n\n a = %d, pti = %d", a, pti);
+    printf("\n a = %d, pti = %d", a, *pti);
+
+    return 0;
+}
