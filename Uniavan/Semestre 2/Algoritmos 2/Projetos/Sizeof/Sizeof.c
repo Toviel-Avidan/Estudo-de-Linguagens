1adcfa03021d0585df918dac85125aa63f7c05ba
Samuel Rozini Hirt <Samuel_rozini@hotmail.com>
2025-03-28 21:46:27 -0300

    Add files via upload

---
+++ Uniavan/Semestre 2/Algoritmos 2/Projetos/Sizeof/Sizeof.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int numeroInt = 45;
+    printf("\nO tamanho da variavel numeroInt é %u bytesz\n", sizeof(numeroInt));
+
+    double numeroDouble = 3.14;
+    printf("\nO tamanho da variavel numeroDouble é %u bytesz\n", sizeof(numeroDouble));
+
+    char letraChar = 'letra';
+    printf("\nO tamanho da variavel letraChar é %u bytesz\n", sizeof(letraChar));
+
+    return 0;
+}
