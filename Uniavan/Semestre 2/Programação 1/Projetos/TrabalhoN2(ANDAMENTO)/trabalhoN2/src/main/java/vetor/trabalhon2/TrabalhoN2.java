2b26e803ba0be9877e6f3910ae82ef526e2a611a
Samuel Rozini Hirt <Samuel_rozini@hotmail.com>
2025-06-03 21:35:38 -0300

    Update TrabalhoN2.java

--- Uniavan/Semestre 2/Programação 1/Projetos/TrabalhoN2(ANDAMENTO)/trabalhoN2/src/main/java/vetor/trabalhon2/TrabalhoN2.java
+++ Uniavan/Semestre 2/Programação 1/Projetos/TrabalhoN2(ANDAMENTO)/trabalhoN2/src/main/java/vetor/trabalhon2/TrabalhoN2.java
@@ -5,13 +5,18 @@ package vetor.trabalhon2;
 import Classes.*;
 import java.util.Scanner;
 
+
 public class TrabalhoN2 {
+    
+    public class Constantes {
+    public static final int PROD_MAX = 3;
+    }
 
     public static void main(String[] args) {
         
         Scanner entrada = new Scanner( System.in );
         
-        Produto[] produtos = new Produto[10];
+        Produto[] produtos = new Produto[Constantes.PROD_MAX];
         
         char escolhaMenu = 'A';
         
@@ -22,10 +27,13 @@ public class TrabalhoN2 {
         double tempPreco;
         int    tempQtd;
         
+        int temp = 0;
+        int existe = 0;
+        
         
         while( escolhaMenu != 'f' && escolhaMenu != 'F' )
         {
-            System.out.printf("\n[]=========================[MENU PRINCIPAL]=========================[]\n\n\n");
+            System.out.printf("\n\n\n[]=========================[MENU PRINCIPAL]=========================[]\n\n\n");
             
             System.out.printf("\n[]==[Precione 'I']==");
             System.out.printf("==[Para incluir um produto]\n");
@@ -60,32 +68,49 @@ public class TrabalhoN2 {
                     while(true)
                     {
                         
-                        System.out.println("\n[]==[Digite o codigo do produto]=[entre 1 e 999]: ");
-                        tempCodigo = entrada.nextInt(); 
+                        existe = 0;
                         
-                        if(tempCodigo >= 1 && tempCodigo <= 999)
+                        while(true)
                         {
+                            System.out.printf("\n[]==[Digite o codigo do produto]=[entre 1 e 999]: ");
+                            tempCodigo = entrada.nextInt(); 
+                        
+                            if(tempCodigo >= 1 && tempCodigo <= 999)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                System.out.printf("\n[]==[O numero digitado foi invalido, tente novamente]\n");
+                            }
+                        }
+                        
+                        for(int i = 0; i < Constantes.PROD_MAX; i++)
+                        {
+                            if(produtos[i] != null)
+                            {
+                                if(produtos[i].getCodigoProduto() == tempCodigo)
+                                {
+                                    System.out.printf("\n[]==[O codigo digitado ja existe digite novamente]");
+                                    existe = 1;
+                                    break;
+                                }
+                            }
+                        }
+                        
+                        if(existe == 0)
                             break;
-                        }
-                        else
-                        {
-                            System.out.println("\n[]==[O numero digitado foi invalido, tente novamente]\n");
-                        }
                     }
                     
-                    while(true)
-                    {
-                        entrada.nextLine();
+                    entrada.nextLine();
                         
-                        System.out.println("[]==[Digite o nome do produto]: ");
-                        tempNome = entrada.next();
-                        break;
-                    }
+                    System.out.printf("\n[]==[Digite o nome do produto]: ");
+                    tempNome = entrada.nextLine();
                     
                     while(true)
                     {
                     
-                        System.out.println("[]==[Digite o preco do produto]: ");
+                        System.out.printf("\n[]==[Digite o preco do produto]: ");
                         tempPreco = entrada.nextDouble();
                         
                         if(tempPreco > 0)
@@ -94,14 +119,14 @@ public class TrabalhoN2 {
                         }
                         else
                         {
-                            System.out.println("\n[]==[O preco digitado foi invalido, tente novamente]\n");
+                            System.out.printf("\n[]==[O preco digitado foi invalido, tente novamente]\n");
                         }
                     }
                     
                     while(true)
                     {
                         
-                        System.out.println("[]==[Digite a quantidade do produto]: ");
+                        System.out.printf("\n[]==[Digite a quantidade do produto]: ");
                         tempQtd = entrada.nextInt(); 
                         
                         if(tempQtd > 0)
@@ -110,8 +135,24 @@ public class TrabalhoN2 {
                         }
                         else
                         {
-                            System.out.println("\n[]==[A quantidade digitado foi invalida, tente novamente]\n");
+                            System.out.printf("\n[]==[A quantidade digitado foi invalida, tente novamente]\n");
                         }
+                    }
+                    
+                    falha = 1;
+                    
+                    for(int i = 0; i < Constantes.PROD_MAX; i++)
+                    {
+                        if(produtos[i] == null)
+                        {
+                            produtos[i] = new Produto(tempCodigo, tempNome, tempPreco, tempQtd );
+                            falha = 0;
+                        }
+                    }
+                    
+                    if(falha == 1)
+                    {
+                        System.out.printf("\n[]==[Nao existe espaço para novos produtos]");
                     }
              
                 break;
@@ -121,7 +162,7 @@ public class TrabalhoN2 {
                     
                     falha = 1;
                     
-                    for(int i = 0; i < 10; i++)
+                    for(int i = 0; i < Constantes.PROD_MAX; i++)
                     {
                         if(produtos[i] != null)
                         {
@@ -131,48 +172,172 @@ public class TrabalhoN2 {
                     }
                     
                     if(falha == 1)
-                        System.out.println("\n[]===[Nenhum produto foi encontrado]");
+                        System.out.printf("\n[]===[Nenhum produto foi encontrado]");
                         
                 break;
                     
                 case 'Q':
                 case 'q':
                     
-                    System.out.println("\n[]==[Qual o codigo do produto que deseja verificar?]: ");
+                    System.out.printf("\n[]==[Qual o codigo do produto que deseja verificar?]: ");
                     tempCodigo = entrada.nextInt();
+                    
                     falha = 1;
+
                     
-                    //VERIFICAÇÃO
-                    
-                    for(int i = 0; i < 10; i++)
+                    for(int i = 0; i < Constantes.PROD_MAX; i++)
                     {
                         if(produtos[i] != null)
                         {
                             if(produtos[i].getCodigoProduto() == tempCodigo)
                             {
-                                System.out.println("\n[]=[A quantidade atual do produto escolhido e]: " + produtos[i].getQtdProdutoEstoque() );
+                                System.out.printf("\n[]=[A quantidade atual do produto escolhido e]: " + produtos[i].getQtdProdutoEstoque() );
                                 falha = 0;
+                                break;
                             }
                         }
                     }
                     
                     if(falha == 1)
-                        System.out.println("\n[]===[O produto nao foi encontrado]");
+                        System.out.printf("\n[]===[O produto nao foi encontrado]");
                         
                 break;
                     
                 case 'V':
                 case 'v':
+                    
+                    System.out.printf("\n[]==[Qual o codigo do produto que deseja vender?]: ");
+                    tempCodigo = entrada.nextInt();
+                    
+                    falha = 1;
+                    
+                    for(int i = 0; i < Constantes.PROD_MAX; i++)
+                    {
+                        if(produtos[i] != null)
+                        {
+                            if(produtos[i].getCodigoProduto() == tempCodigo)
+                            {
+                                
+                                while(true)
+                                {
+                                    System.out.printf("\n[]==[Produto encontrado, quanto deseja vender?]: ");
+                                    temp = entrada.nextInt();
+                                    
+                                    if(temp <= 0)
+                                    {
+                                        System.out.printf("\n[]==[Valor invalido digite novamente]");
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+                                }
+                                
+                                if(produtos[i].getQtdProdutoEstoque() >= temp)
+                                {
+                                    produtos[i].venderProduto(temp);
+                                    System.out.printf("\n[]==[O produto foi vendido]");
+                                    falha = 0;
+                                    break;
+                                }
+                                else
+                                {
+                                    System.out.printf("\n[]==[O produto esta sem estoque para a compra]");
+                                    falha = 0;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    
+                    if(falha == 1)
+                        System.out.printf("\n[]===[O produto nao foi encontrado]");
                         
                 break;
                     
                 case 'R':
                 case 'r':
+                    
+                    System.out.printf("\n[]==[Qual o codigo do produto que deseja reabastecer?]: ");
+                    tempCodigo = entrada.nextInt();
+                    
+                    falha = 1;
+                    
+                    for(int i = 0; i < Constantes.PROD_MAX; i++)
+                    {
+                        if(produtos[i] != null)
+                        {
+                            if(produtos[i].getCodigoProduto() == tempCodigo)
+                            {
+                                
+                                while(true)
+                                {
+                                    System.out.printf("\n[]==[Produto encontrado, quanto deseja reabastecer?]: ");
+                                    temp = entrada.nextInt();
+                                    
+                                    if(temp <= 0)
+                                    {
+                                        System.out.printf("\n[]==[Valor invalido digite novamente]");
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+                                }
+                                
+                                produtos[i].reabastecerProduto(temp);
+                                System.out.printf("\n[]==[O produto foi reabastecido]");
+                                falha = 0;
+                                break;
+                            }
+                        }
+                    }
+                    
+                    if(falha == 1)
+                        System.out.printf("\n[]===[O produto nao foi encontrado]");
                         
                 break;
                     
                 case 'D':
                 case 'd':
+                    
+                    System.out.printf("\n[]==[Qual o codigo do produto que deseja aplicar o desconto?]: ");
+                    tempCodigo = entrada.nextInt();
+                    
+                    falha = 1;
+                    
+                    for(int i = 0; i < Constantes.PROD_MAX; i++)
+                    {
+                        if(produtos[i] != null)
+                        {
+                            if(produtos[i].getCodigoProduto() == tempCodigo)
+                            {
+                                
+                                while(true)
+                                {
+                                    System.out.printf("\n[]==[Produto encontrado, quanto desconto deseja aplicar?]=[entre 1 e 90 porcento]: ");
+                                    temp = entrada.nextInt();
+                                    
+                                    if(temp >= 1 && temp <= 90)
+                                    {
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        System.out.printf("\n[]==[Valor invalido digite novamente]");
+                                    }
+                                }
+                                
+                                produtos[i].aplicarDesconto(temp);
+                                System.out.printf("\n[]==[O desconto foi aplicado]");
+                                falha = 0;
+                                break;
+                            }
+                        }
+                    }
+                    
+                    if(falha == 1)
+                        System.out.printf("\n[]===[O produto nao foi encontrado]");
                         
                 break;
                     
@@ -183,12 +348,10 @@ public class TrabalhoN2 {
                 
                 default:
                     
-                    System.out.println("\n\n\n[]==[ESCOLHA INVALIDA TENTE NOVAMENTE]\n\n");
+                    System.out.printf("\n\n\n[]==[ESCOLHA INVALIDA TENTE NOVAMENTE]\n\n");
                     
                 break;
             }
-                    
-        //produtos[ i ] = new Produto( tempMatricula , tempNome , tempQtdeNotas , tempNotas );
         }
     }
 }
